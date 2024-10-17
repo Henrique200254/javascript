6f2b4fa62636f70b5ef841313934090f2ea7bd0a
Henrique Castilho <Henriqueribeiro200254@hotmail.com>
2024-10-17 11:43:53 -0300

    Fim do projeto POO de Javascript

--- projetoPoo/script.js
+++ projetoPoo/script.js
@@ -2,13 +2,77 @@ class Produto {
     constructor () {
       this.id = 1
       this.arrayproduto = []
+      this.editID = null
     }
     salvar () {
        let produto = this.lerDados()
        console.log(produto)
        if (this.validaCampos(produto) == true) {
-           alert('salvar')
+           if (this.editID === null) {
+                this.adicionar(produto)
+           } else {
+                this.atualizar(this.editID, produto)
+           }
+        
        }
+       this.listaTabela()
+       this.cancelar()
+    }
+
+    listaTabela() {
+        let tbody = document.querySelector('tbody#tbody')
+        tbody.innerText = ''
+        for (let i = 0; i < this.arrayproduto.length; i++) {
+            let tr = tbody.insertRow()
+
+            let td_id = tr.insertCell()
+            let td_produto = tr.insertCell()
+            let td_valor = tr.insertCell()
+            let td_acoes = tr.insertCell()
+
+            td_id.innerText = this.arrayproduto[i].id
+            td_produto.innerText = this.arrayproduto[i].nome
+            td_valor.innerText = this.arrayproduto[i].valor
+
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/favicon.png';
+            imgEdit.setAttribute("onclick", "produto.editar(" + JSON.stringify(this.arrayproduto[i]) +")")
+
+            let imgDelete = document.createElement('img')
+            imgDelete.src = 'img/excluir.png'
+            imgDelete.setAttribute("onclick", "produto.deletar(" + this.arrayproduto[i].id + ")")
+            
+            td_acoes.appendChild(imgDelete)
+            td_acoes.appendChild(imgEdit);
+
+            console.log(this.arrayproduto)
+            
+        }
+    }
+
+    adicionar(produto) {
+        produto.valor = parseFloat(produto.valor)
+       this.arrayproduto.push(produto)
+       this.id++
+    }
+
+    atualizar(id, produto) {
+       for (let i = 0; i < this.arrayproduto.length; i++) {
+        if (this.arrayproduto[i].id == id) {
+            this.arrayproduto[i].nome = produto.nome
+            this.arrayproduto[i].valor = produto.valor
+        }
+       }
+    }
+
+    editar(dados) {
+        this.editID = dados.id
+
+        document.querySelector('input#produto').value = dados.nome
+        document.querySelector('input#valor').value = dados.valor
+        
+        document.getElementById('btn1').innerText = 'Atualizar'
     }
 
     lerDados() {
@@ -42,7 +106,27 @@ class Produto {
     }
 
     cancelar() {
+        document.getElementById('produto').value = ''
+        document.getElementById('valor').value = ''
+
+        document.getElementById('btn1').innerText = 'Salvar'
+        this.editID = null
         
+    }
+
+    deletar(id) {
+        if (confirm('Deseja realmente deletar o produto do ID' + id)) {
+             
+        
+          let tr = tbody.insertRow()
+          for (let i = 0; i < this.arrayproduto.length; i++) {
+            if (this.arrayproduto[i].id == id) {
+                 this.arrayproduto.splice(i, 1)
+                 tbody.deleteRow(i)
+            }
+          }
+          console.log(this.arrayproduto)
+        }
     }
 }
 
