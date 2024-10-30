283c73721edf4648201d76c1b718e602a421f409
Henrique Castilho <Henriqueribeiro200254@hotmail.com>
2024-10-30 10:52:57 -0300

    Acréscimo em Objetos

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -7,3 +7,14 @@ let amigo = {nome: 'José', peso: 85.4, sexo: 'M',
 amigo.engordar(2)
 console.log( amigo)
 console.log(` ${amigo.nome} pesa ${amigo.peso}kg`)
+
+const carro = {marca: "ford", modelo: "ka", ano: 2015, placa: "ABC-1234", buzina: function () {let texto = "biiiii"
+  return texto
+},
+completo: function () {
+   return "A marca é "+ this.marca + " e o modelo é " + this.modelo
+}
+}
+// para pegar o valor de marca e jogar na propriedade completo usamos o This, que é um elemento de classe para chamar elementos e funções. Parece que quando eu coloco uma função dentro de uma propriedade de objeto ela funciona como classe.
+console.log(carro.buzina())
+console.log(carro.completo())
